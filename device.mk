b6dfd70ff2ec303fabeb2d87643e3a70ceae6464
Matheus-TestUser1 <99508668+Matheus-TestUser1@users.noreply.github.com>
2024-06-21 13:56:50 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,7 +1,11 @@
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 33
 
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 33
