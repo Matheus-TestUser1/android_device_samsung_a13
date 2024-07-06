ddb1890be86aa53a7566b2cf44d4eae6cc8290b1
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2024-07-06 16:12:43 -0300

    Update twrp_a13.mk
    Signed-off-by: Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>

--- twrp_a13.mk
+++ twrp_a13.mk
@@ -19,8 +19,6 @@ DEVICE_PATH := device/samsung/$(DEVICE_CODENAME)
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/a13/device.mk)
