b80c7203ba3e285016af402d5a8e073eabb352f4
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2024-08-01 22:27:12 -0300

    Update BoardConfig.mk
    Signed-off-by: Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,11 +13,15 @@ TARGET_USES_UEFI := true
 # Arquitetura do dispositivo
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a55
+
 TARGET_2ND_ARCH := arm64
 TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := arm64-v8a
+TARGET_2ND_CPU_ABI2 :=
 TARGET_2ND_CPU_VARIANT := cortex-a55
-
 
 TARGET_CPU_SMP := true
 
